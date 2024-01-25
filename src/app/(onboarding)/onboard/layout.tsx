a32b4c8ee9688313d79cdbdb080bfb8e233d355b
namminimi <tbvhdaos@naver.com>
2024-01-25 23:16:17 +0900

    chore: onboard -> onboarding으로 파일명 변경

--- src/app/(onboarding)/onboard/layout.tsx
+++ src/app/(onboarding)/onboard/layout.tsx
@@ -1,5 +1,5 @@
-import OnBoardingHeader from '../_components/OnBoardingHeader';
 import { LayoutType } from '../../_types';
+import OnBoardingHeader from '../_components/OnBoardingHeader';
 
 function Layout({ children }: LayoutType) {
   return (
