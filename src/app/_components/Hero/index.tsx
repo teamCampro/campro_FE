626c15e5c26a48eaa032f185c3710d8e04116093
lsc58461 <lsc58461@naver.com>
2024-01-29 15:56:41 +0900

    fix: css 수정

--- src/app/_components/Hero/index.tsx
+++ src/app/_components/Hero/index.tsx
@@ -16,14 +16,14 @@ function Hero() {
   return (
     <div className='relative flex items-center justify-center'>
       <Image
-        className='h-486pxr w-full object-cover tablet:h-501pxr desktop:h-468pxr'
+        className='h-468pxr w-full object-cover mobile:h-486pxr tablet:h-501pxr'
         width={1200}
         height={468}
         src={heroImage}
         alt='히어로 이미지'
       />
-      <div className='flex-center absolute top-62pxr flex-col  gap-28pxr tablet:top-64pxr'>
-        <h1 className='text-center text-white font-title3-bold tablet:font-h1 '>
+      <div className='flex-center absolute top-64pxr flex-col gap-28pxr mobile:top-62pxr'>
+        <h1 className='text-center text-white font-h1 mobile:font-title3-bold '>
           나의 취향에 맞는
           <br />
           캠핑장을 쉽게 찾아보세요.
