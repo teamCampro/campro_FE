1c78c6219cc0227f94da092dd9815b6ad702fe99
minhyeokG0 <yeppi9801@gmail.com>
2024-02-01 01:28:04 +0900

    feat/#49: 지도 검색 페이지 페이지네이션 구현

--- src/app/(header&footer)/_components/SearchPagination.tsx
+++ src/app/(header&footer)/_components/SearchPagination.tsx
@@ -3,12 +3,30 @@
 import { IconNavigationLeft, IconNavigationRight } from '@/public/svgs';
 import { useState } from 'react';
 const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-function Pagination() {
+
+interface Props {
+  currentPage: number;
+  totalItems: number;
+  onUpdatePage: (pageNumber: number) => void;
+}
+
+function SearchPagination({ currentPage, totalItems, onUpdatePage }: Props) {
   const [selected, setSelected] = useState(1);
+
+  const handleClickPage = (p: number) => {
+    setSelected(p);
+    onUpdatePage(p);
+  };
+
+  const pages = Array.from(
+    { length: Math.ceil(totalItems / 18) },
+    (x, i) => i + 1,
+  );
+
   return (
     <div className='flex-center gap-20pxr text-gray500'>
-      <button type='button'>
-        <IconNavigationLeft />
+      <button type='button' disabled={currentPage === 1 ? true : false}>
+        <IconNavigationLeft onClick={() => handleClickPage(currentPage - 1)} />
       </button>
       <div>
         {pages.map((page) => (
@@ -16,17 +34,20 @@ function Pagination() {
             key={page}
             type='button'
             className={`px-8pxr py-2pxr font-caption1 ${selected === page ? 'rounded-xl bg-gray100 text-gray800 font-caption1-semibold' : ''}`}
-            onClick={() => setSelected(page)}
+            onClick={() => handleClickPage(page)}
           >
             {page}
           </button>
         ))}
       </div>
-      <button type='button'>
-        <IconNavigationRight />
+      <button
+        type='button'
+        disabled={currentPage === Math.ceil(totalItems / 18) ? true : false}
+      >
+        <IconNavigationRight onClick={() => handleClickPage(currentPage + 1)} />
       </button>
     </div>
   );
 }
 
-export default Pagination;
+export default SearchPagination;
