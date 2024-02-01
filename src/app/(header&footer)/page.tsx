0094119bcfeea86c45c0ffc873763c49a9c79645
aowjarkwk <justgam12@gmail.com>
2024-02-01 14:48:04 +0900

    fix: 모바일 화면의 패딩 수정

--- src/app/(header&footer)/page.tsx
+++ src/app/(header&footer)/page.tsx
@@ -3,7 +3,7 @@ function page() {
   return (
     <div>
       <Hero />
-      <div className='flex w-full flex-col bg-gray-100 pl-40pxr pt-104pxr'>
+      <div className='flex w-full flex-col bg-gray-100 pl-40pxr pt-104pxr mobile:pl-16pxr'>
         <div className='flex-center'>
           <CategoryList />
         </div>
