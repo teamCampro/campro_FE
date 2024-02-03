41db75816300590e7231cb28b2d84de460dd9ae0
aowjarkwk <justgam12@gmail.com>
2024-02-03 20:14:34 +0900

    [#54] feat: 모바일 화면에서 목록+지도 버튼이 보이지 않도록 수정

--- src/app/(header)/search/_components/MapSizeButtons.tsx
+++ src/app/(header)/search/_components/MapSizeButtons.tsx
@@ -1,28 +1,44 @@
 'use client';
 
 import useDropdown from '@/hooks/useDropdown';
+import useMediaQueries from '@/hooks/useMediaQueries';
 import { IconArrowDown, IconArrowUp } from '@/public/svgs';
 import { MAP_SIZES } from '@/src/app/_constants';
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { MapSizeType } from '../page';
 
 interface Props {
   handleMapSize: (mapSize: MapSizeType) => void;
 }
 
-function MapSizeButtons({ handleMapSize }: Props) {
-  const [mainButton, setMainButton] = useState(MAP_SIZES[0].text);
-  const { isOpen, toggleDropdown, wrapperRef } = useDropdown();
+interface MapSizesProps {
+  text: string;
+  mobileText: string;
+  mapSize: MapSizeType;
+  icon: ReactNode;
+}
 
-  const handleAdditionalButtonClick = ({
-    text,
-    mapSize,
-  }: {
-    text: string;
-    mapSize: MapSizeType;
-  }) => {
-    setMainButton(text);
-    handleMapSize(mapSize);
+function MapSizeButtons({ handleMapSize }: Props) {
+  const [selectedMapSize, setSelectedMapSize] = useState(MAP_SIZES[0]);
+  const { isOpen, toggleDropdown, closeDropdown, wrapperRef } = useDropdown();
+
+  const mobileMediaQuery = useMediaQueries({ breakpoint: 767 })?.mediaQuery
+    .matches;
+  const isMobile = typeof window !== 'undefined' ? mobileMediaQuery : true;
+
+  const handleAdditionalButtonClick = (mapSize: MapSizesProps) => {
+    setSelectedMapSize(mapSize);
+    handleMapSize(mapSize.mapSize);
+    closeDropdown();
+  };
+
+  const shouldHideItem = (map: MapSizesProps) => {
+    if (isMobile) {
+      return (
+        map.mobileText === selectedMapSize.mobileText || map.mapSize === 'half'
+      );
+    }
+    return map.mapSize === selectedMapSize.mapSize;
   };
 
   return (
@@ -32,31 +48,29 @@ function MapSizeButtons({ handleMapSize }: Props) {
     >
       <ul className='flex w-127pxr flex-col gap-8pxr'>
         {isOpen &&
-          MAP_SIZES.map(
-            ({ text, mapSize, icon }) =>
-              text !== mainButton && (
-                <li key={text}>
-                  <button
-                    type='button'
-                    onClick={() =>
-                      handleAdditionalButtonClick({ text, mapSize })
-                    }
-                    className={`flex-center h-48pxr w-full gap-4pxr text-nowrap rounded-full border border-gray300 bg-white py-12pxr pl-20pxr pr-14pxr text-gray600 font-body2 hover:text-primary100 mobile:text-gray700`}
-                  >
-                    {text}
-                    <span className='ml-auto'>{icon}</span>
-                  </button>
-                </li>
-              ),
-          )}
+          MAP_SIZES.map((map) => (
+            <li
+              key={map.mapSize}
+              className={shouldHideItem(map) ? 'hidden' : ''}
+            >
+              <button
+                type='button'
+                onClick={() => handleAdditionalButtonClick(map)}
+                className={`flex-center h-48pxr w-full gap-4pxr text-nowrap rounded-full border border-gray300 bg-white py-12pxr pl-20pxr pr-14pxr text-gray600 font-body2 hover:text-primary100 mobile:text-gray700`}
+              >
+                {isMobile ? map.mobileText : map.text}
+                <span className='ml-auto'>{map.icon}</span>
+              </button>
+            </li>
+          ))}
       </ul>
       <button
         type='button'
         onClick={toggleDropdown}
-        className={`flex-center h-48pxr w-full gap-4pxr text-nowrap rounded-full border border-gray300 bg-white py-12pxr pl-20pxr pr-14pxr text-gray600 transition-all font-body2 mobile:text-gray700 ${isOpen && 'border border-primary100 text-primary100'}`}
+        className={`flex-center h-48pxr w-full gap-4pxr text-nowrap rounded-full border border-gray300 bg-white py-12pxr pl-20pxr pr-14pxr text-gray600 transition-all font-body2 mobile:text-gray700 ${isOpen && 'border border-primary100 text-primary100 mobile:text-primary100'}`}
       >
-        {mainButton}
-        <span className='ml-auto'>
+        {isMobile ? selectedMapSize.mobileText : selectedMapSize.text}
+        <span className='pointer-events-none ml-auto'>
           {isOpen ? <IconArrowDown /> : <IconArrowUp />}
         </span>
       </button>
--- src/app/_constants/mapSizes.tsx
+++ src/app/_constants/mapSizes.tsx
@@ -2,10 +2,30 @@ import { IconSearchHalf, IconSearchList, IconSearchMap } from '@/public/svgs';
 import { ReactNode } from 'react';
 import { MapSizeType } from '../(header)/search/page';
 
-const MAP_SIZES: { text: string; mapSize: MapSizeType; icon: ReactNode }[] = [
-  { text: '목록+지도', mapSize: 'half', icon: <IconSearchHalf /> },
-  { text: '목록만', mapSize: 'list', icon: <IconSearchList /> },
-  { text: '지도만', mapSize: 'map', icon: <IconSearchMap /> },
+const MAP_SIZES: {
+  text: string;
+  mobileText: string;
+  mapSize: MapSizeType;
+  icon: ReactNode;
+}[] = [
+  {
+    text: '목록+지도',
+    mobileText: '목록만',
+    mapSize: 'half',
+    icon: <IconSearchHalf />,
+  },
+  {
+    text: '목록만',
+    mobileText: '목록만',
+    mapSize: 'list',
+    icon: <IconSearchList />,
+  },
+  {
+    text: '지도만',
+    mobileText: '지도만',
+    mapSize: 'map',
+    icon: <IconSearchMap />,
+  },
 ];
 
 export default MAP_SIZES;
