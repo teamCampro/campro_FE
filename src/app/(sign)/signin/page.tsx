efd35331318f976db4b403bb0cb75a6c0fe8e978
minhyeokG0 <yeppi9801@gmail.com>
2024-02-05 18:35:17 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/app/(sign)/signin/page.tsx
+++ src/app/(sign)/signin/page.tsx
@@ -1,25 +1,30 @@
 import LoginForm from '@/components/Form/LoginForm';
 import Image from 'next/image';
 import signImage from '@/public/avifs/sign.avif';
+import KakaoIcon from '@/public/svgs/Kakao.svg';
+import NaverIcon from '@/public/svgs/naver.svg';
+import GoogleIcon from '@/public/svgs/Google.svg';
+
 function Page() {
   return (
-    <div className='relative flex  h-screen w-full items-center justify-center'>
+    <div className='relative flex h-1250pxr w-full items-center justify-center overflow-hidden'>
       <Image
-        className='z-0 h-full w-full bg-cover bg-center bg-no-repeat'
+        className=' z-0 h-full w-full overflow-hidden bg-cover bg-center bg-no-repeat'
         src={signImage}
         alt='로그인 페이지 배경이미지'
         priority
       />
       <div className='absolute top-0pxr flex  w-full flex-col items-center justify-center gap-56pxr px-206pxr pb-149pxr pt-102pxr'>
-        <div className='flex w-full flex-col justify-center'>
-          <p className='font-sign-title '>CAMPRO</p>
-          <p className='font-sign-subTitle1'>
-            Camping for <span className='font-sign-subTitle2'>Beginers</span>
+        <div className='flex w-full flex-col items-center justify-center'>
+          <p className='flex-center flex w-full font-sign-title '>CAMPRO</p>
+          <p className='flex-center flex-nowrap gap-12pxr whitespace-nowrap font-sign-subTitle1'>
+            Camping for
+            <span className='font-sign-subTitle2'>Beginers</span>
           </p>
         </div>
-        <div className='flex w-full flex-col items-center  justify-center px-194pxr'>
+        <div className='flex flex-col items-center justify-center px-194pxr'>
           <LoginForm />
-          <div className=' mt-24pxr flex w-full max-w-400pxr justify-around text-gray300 font-body2'>
+          <div className=' mt-24pxr flex w-full justify-around text-gray300 font-body2'>
             <span>회원가입</span>
             <span>아이디/비밀번호 찾기</span>
           </div>
@@ -29,6 +34,20 @@ function Page() {
               간편 로그인
             </span>
             <div className='h-0pxr w-143pxr border border-b-white'></div>
+          </div>
+          <div className='flex w-full flex-col items-center justify-center gap-16pxr'>
+            <button className='flex-center flex h-56pxr w-full rounded-[8px] bg-[#fee500] px-14pxr py-0pxr '>
+              <KakaoIcon />
+              <span className='px-86pxr font-body1'>카카오 로그인</span>
+            </button>
+            <button className='flex-center flex h-56pxr w-full rounded-[8px] bg-[#03C75A] px-14pxr py-0pxr '>
+              <NaverIcon />
+              <span className='px-86pxr font-body1'>네이버 로그인</span>
+            </button>
+            <button className='flex-center flex h-56pxr w-full rounded-[8px] bg-[#F2F2F2] px-14pxr py-0pxr '>
+              <GoogleIcon />
+              <span className='px-86pxr font-body1'>네이버 로그인</span>
+            </button>
           </div>
         </div>
       </div>
--- src/app/_components/Form/LoginForm.tsx
+++ src/app/_components/Form/LoginForm.tsx
@@ -16,49 +16,45 @@ import {
 
 function LoginForm() {
   return (
-    <div className='flex-center w-full max-w-400pxr '>
-      <CommonForm
-        mode='onBlur'
-        className=' flex w-full flex-col gap-48pxr'
-        defaultValues={{ email: '', password: '' }}
-        onSubmit={(data: FieldValues) => console.log(data)}
-      >
-        <div className='flex w-full flex-col gap-24pxr'>
-          <InputContainer className='flex  flex-col gap-8pxr'>
-            <Label
-              className='flex w-full text-gray400 font-body2'
-              htmlFor='email'
-            >
-              이메일
-            </Label>
-            <CommonInput
-              name='email'
-              placeholder='이메일을 입력해주세요'
-              rules={emailValidate}
-            />
-            <ErrorMessage name='email' />
-          </InputContainer>
+    <CommonForm
+      mode='onBlur'
+      className=' flex w-full flex-col gap-48pxr'
+      defaultValues={{ email: '', password: '' }}
+      onSubmit={(data: FieldValues) => console.log(data)}
+    >
+      <div className='flex w-full flex-col gap-24pxr '>
+        <InputContainer className='flex w-full flex-col gap-8pxr'>
+          <Label className='flex text-gray400 font-body2' htmlFor='email'>
+            이메일
+          </Label>
+          <CommonInput
+            name='email'
+            placeholder='이메일을 입력해주세요'
+            rules={emailValidate}
+          />
+          <ErrorMessage name='email' />
+        </InputContainer>
 
-          <InputContainer className='flex w-full flex-col gap-8pxr'>
-            <Label
-              className='flex w-full text-gray400 font-body2'
-              htmlFor='password'
-            >
-              비밀번호
-            </Label>
-            <CommonInput
-              name='password'
-              placeholder='비밀번호를 입력해주세요'
-              rules={passwordValidate}
-            />
-            <ErrorMessage name='password' />
-          </InputContainer>
-        </div>
-        <Button.Round size='md' type='submit' custom='flex w-full'>
-          로그인
-        </Button.Round>
-      </CommonForm>
-    </div>
+        <InputContainer className='flex w-full flex-col gap-8pxr'>
+          <Label className='flex  text-gray400 font-body2' htmlFor='password'>
+            비밀번호
+          </Label>
+          <CommonInput
+            name='password'
+            placeholder='비밀번호를 입력해주세요'
+            rules={passwordValidate}
+          />
+          <ErrorMessage name='password' />
+        </InputContainer>
+      </div>
+      <Button.Round
+        size='md'
+        type='submit'
+        custom='flex w-full  bg-primary100 font-title3-bold text-white'
+      >
+        로그인
+      </Button.Round>
+    </CommonForm>
   );
 }
 
