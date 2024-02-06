5d1eab5a22440934f3885da79c8ffdaec2124e0b
namminimi <tbvhdaos@naver.com>
2024-02-07 03:43:51 +0900

    feat: 카카오지도, 마커표시, 마커이미지 변경 구현

--- src/app/(header&footer)/_components/MinikakaoMap.tsx
+++ src/app/(header&footer)/_components/MinikakaoMap.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import createMarkerImage from '../../_utils/createMarkerImage';
 
 interface KakaoMap {
   // 여기에 kakao 맵 관련 타입들을 정의하세요.
@@ -12,54 +13,88 @@ declare global {
   }
 }
 
-const { kakao } = window;
-
 function MinikakaoMap() {
   const mapRef = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<kakao.maps.Map | null>(null);
 
   useEffect(() => {
+    if (typeof window === 'undefined' || !window.kakao) return;
+    const { kakao } = window;
+
     const container = document.getElementById('map') as HTMLDivElement; //지도를 담을 영역의 DOM 레퍼런스
-    const options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
 
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-    // 주소-좌표 변환 객체를 생성합니다
-    const geocoder = new kakao.maps.services.Geocoder();
+    window.kakao.maps.load(() => {
+      const options = {
+        //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        level: 3, //지도의 레벨(확대, 축소 정도)
+      };
 
-    // 주소로 좌표를 검색합니다
-    geocoder.addressSearch(
-      '달천도담길 3-40',
-      function (result: any, status: any) {
-        // 정상적으로 검색이 완료됐으면
-        if (status === kakao.maps.services.Status.OK) {
-          const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+      if (!mapRef.current) return;
+      const kakaomap = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+      setMap(kakaomap);
+      console.log(kakaomap);
+      console.log(kakao);
+      if (!kakaomap) return;
 
-          // 결과값으로 받은 위치를 마커로 표시합니다
-          const marker = new kakao.maps.Marker({
-            map: map,
-            position: coords,
-          });
+      // 주소-좌표 변환 객체를 생성합니다
+      const geocoder = new kakao.maps.services.Geocoder();
+      /* if (!geocoder) return; */
+      // 주소로 좌표를 검색합니다
 
-          // 인포윈도우로 장소에 대한 설명을 표시합니다
-          const infowindow = new kakao.maps.InfoWindow({
-            content:
-              '<div style="width:150px;text-align:center;padding:6px 0;">아잉스크림</div>',
-          });
-          infowindow.open(map, marker);
+      geocoder.addressSearch(
+        '달천도담길 3-40',
+        function (result: any, status: any) {
+          // 정상적으로 검색이 완료됐으면
+          if (status === kakao.maps.services.Status.OK) {
+            const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            const imageSrc =
+              'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png'; // 마커이미지의 주소입니다
+            const imageSize = new kakao.maps.Size(64, 69); // 마커이미지의 크기입니다
+            const imageOption = { offset: new kakao.maps.Point(27, 69) };
 
-          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-          map.setCenter(coords);
-        }
-      },
-    );
+            const markerImage = new kakao.maps.MarkerImage(
+              imageSrc,
+              imageSize,
+              imageOption,
+            );
+
+            const clickedMarkerImage = createMarkerImage({
+              src: '/svgs/markerGreen.svg',
+              size: { width: 19, height: 25 },
+            });
+
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            const marker = new kakao.maps.Marker({
+              map: kakaomap,
+              position: coords,
+              image: clickedMarkerImage,
+            });
+
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            const infowindow = new kakao.maps.InfoWindow({
+              content:
+                '<div style="width:150px;text-align:center;padding:6px 0;">우리집</div>',
+            });
+            infowindow.open(kakaomap, marker);
+
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            kakaomap.setCenter(coords);
+          }
+        },
+      );
+    });
+    console.log(map);
   }, []);
 
+  useEffect(() => {
+    if (!map) return;
+    map.relayout();
+  }, [map]);
+
   return (
-    <div>
-      <div ref={mapRef} id='map'></div>
+    <div className='flex-center w-full'>
+      <div ref={mapRef} id='map' className='h-400pxr w-500pxr'></div>
     </div>
   );
 }
