91d2f2a8c22decf28f9c96731c77a23826502247
minhyeokG0 <yeppi9801@gmail.com>
2024-02-07 01:22:13 +0900

    feat: 캠핑장 이미지 모달 UI 구현

--- src/app/(header&footer)/_components/CampImage/CampImageCarousel.tsx
+++ src/app/(header&footer)/_components/CampImage/CampImageCarousel.tsx
@@ -4,32 +4,41 @@ import 'swiper/css/pagination';
 import { CampImageData } from '.';
 import Image from 'next/image';
 import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+import { useState } from 'react';
+import ModalAboutCampImage from './ModalAboutCampImage';
 
 function CampImageCarousel({
   campImages,
 }: {
   campImages: CampImageData[] | null;
 }) {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleRenderModal = () => setIsOpenModal(true);
+  const handleCloseModal = () => setIsOpenModal(false);
+
+  const swiperSettings = {
+    className: 'flex w-full max-w-1360pxr',
+    modules: [Navigation, Pagination, A11y, Autoplay],
+    slidesPerView: 1,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: { clickable: true },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+    },
+  };
+
   return (
-    campImages && (
-      <Swiper
-        className='flex w-full max-w-1360pxr'
-        modules={[Navigation, Pagination, A11y, Autoplay]}
-        slidesPerView={1}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        pagination={{ clickable: true }}
-        breakpoints={{
-          320: {
-            slidesPerView: 1,
-          },
-        }}
-      >
-        {campImages.slice(0, 5).map(
-          (item, i) =>
-            item.imgUrl && (
+    <>
+      {campImages && (
+        <Swiper {...swiperSettings}>
+          {campImages.slice(0, 5).map((item, i) =>
+            item.imgUrl ? (
               <SwiperSlide className='flex w-full' key={item.id}>
                 <Image
                   className='flex w-full object-cover'
@@ -37,12 +46,21 @@ function CampImageCarousel({
                   alt={`camp-image${i}`}
                   width={688}
                   height={398}
+                  onClick={handleRenderModal}
                 />
               </SwiperSlide>
-            ),
-        )}
-      </Swiper>
-    )
+            ) : null,
+          )}
+        </Swiper>
+      )}
+
+      {isOpenModal && (
+        <ModalAboutCampImage
+          campImages={campImages}
+          onClose={handleCloseModal}
+        />
+      )}
+    </>
   );
 }
 
--- src/app/(header&footer)/_components/CampImage/CampImageForModal.tsx
+++ src/app/(header&footer)/_components/CampImage/CampImageForModal.tsx
@@ -3,37 +3,60 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import ExitIcon from '@/public/svgs/exit.svg';
 import Image from 'next/image';
 import { CampImageData } from '.';
+import { useState } from 'react';
 function CampImageForModal({ campImages }: { campImages: CampImageData[] }) {
+  const [mainImage, setMainImage] = useState<string>(campImages[0].imgUrl);
+  const [subImages, setSubImages] = useState<CampImageData[]>(campImages);
+
+  console.log(subImages);
+  const handleClickImage = (img: string) => {
+    setMainImage(img);
+    setSubImages(subImages.filter((sub) => sub.imgUrl != mainImage));
+  };
+
   return (
-    <div className='flex w-full max-w-1008pxr flex-col items-start rounded-[16px] bg-white pt-16pxr'>
+    <div className='flex w-full  max-w-1008pxr flex-col items-start rounded-[16px] bg-white pt-16pxr'>
       <div className='flex-center flex w-full gap-16pxr px-20pxr'>
         <ExitIcon />
         <span className='text-black font-title1-bold '> 전체 사진</span>
       </div>
-      <div className='flex w-full flex-col items-center justify-center gap-10pxr bg-gray100'>
+      <div className='flex w-full flex-col items-center justify-center  bg-gray100'>
         <div className=' flex-center w-full'>
-          <Image src={campImages[0].imgUrl} fill alt='메인 캠프 이미지' />
+          {mainImage && (
+            <Image
+              className='flex w-full '
+              src={campImages[0].imgUrl}
+              width={586}
+              height={356}
+              alt='메인 캠프 이미지'
+            />
+          )}
         </div>
 
         <Swiper
-          className='gap10pxr flex items-start bg-white p-16pxr'
-          slidesPerView={1}
+          id='campImage-swiper'
+          className='!flex w-full items-start !gap-12pxr  bg-white !p-16pxr'
+          slidesPerView='auto'
           breakpoints={{
-            320: {
-              slidesPerView: 1,
+            1200: {
+              slidesPerView: 7.5,
+            },
+            768: {
+              slidesPerView: 6,
             },
           }}
         >
-          {campImages.slice(0, 5).map(
+          {subImages.slice(0, 9).map(
             (item, i) =>
               item.imgUrl && (
-                <SwiperSlide className='flex w-full' key={item.id}>
+                <SwiperSlide className='!flex w-full ' key={item.id}>
                   <Image
-                    className='flex w-full object-cover'
+                    className={`flex h-100pxr w-100pxr rounded-[12px] object-cover ${mainImage === item.imgUrl ? 'border-3 border-green-500' : 'border-white'}`}
                     src={item.imgUrl}
                     alt={`camp-image${i}`}
-                    width={688}
-                    height={398}
+                    width={100}
+                    height={100}
+                    onClick={() => handleClickImage(item.imgUrl)}
                   />
                 </SwiperSlide>
               ),
