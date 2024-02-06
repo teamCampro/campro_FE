ea11b89d8c90c645e82457aeed5fef8a256136a4
wecaners <wecaners@naver.com>
2024-02-06 22:38:49 +0900

    feat: 부대시설 페이지 구현

--- src/app/(owner)/_components/OwnerSelectButton.tsx
+++ src/app/(owner)/_components/OwnerSelectButton.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 function OwnerSelectButton({ type, buttonText, children }: Props) {
   const primaryTypeClassName =
-    'bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] h-181pxr w-340pxr rounded-2xl flex flex-col gap-5xpr hover:border-2 hover:border-black justify-center items-start px-33pxr border border-gray500 hover:px-32pxr';
+    'bg-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] h-181pxr w-340pxr rounded-2xl flex flex-col gap-8pxr hover:border-2 hover:border-black justify-center items-start px-33pxr border border-gray500 hover:px-32pxr';
   const smallTypeClassName =
-    'bg-white w-170pxr h-160pxr rounded-2xl shadow-[0_4px_4px_rgba(0,0,0,0.25)] flex flex-col gap-5pxr hover:border-2 justify-center items-start px-33pxr border border-gray500 hover:px-32pxr hover:border-black';
+    'bg-white w-170pxr h-160pxr rounded-2xl shadow-[0_4px_4px_rgba(0,0,0,0.25)] flex flex-col gap-8pxr hover:border-2 justify-center items-start px-33pxr border border-gray500 hover:px-32pxr hover:border-black';
   const buttonClassName = type ? smallTypeClassName : primaryTypeClassName;
 
   return (
---
+++ src/app/(owner)/owner/registration/amenities/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import OwnerTitle from '../../../_components/OwnerTitle';
+import OwnerButtonList from '../../../_components/OwnerButtonList';
+import AMENITIES_CONFIG from '../../../_constants/ownerAmenitiesButtons';
+
+function AmenitiesPage() {
+  return (
+    <div className='flex flex-col items-center gap-110pxr'>
+      <OwnerTitle>사용할 수 있는 부대시설을 알려주세요!</OwnerTitle>
+      <div className='grid-row-auto grid grid-cols-3 gap-x-35pxr gap-y-15pxr'>
+        <OwnerButtonList config={AMENITIES_CONFIG} />
+      </div>
+    </div>
+  );
+}
+
+export default AmenitiesPage;
