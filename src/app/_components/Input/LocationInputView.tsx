f57a3695fdb4d14eaee52ebfe7fe3d7c707b18c5
minhyeokG0 <144667681+minhyeokG0@users.noreply.github.com>
2024-02-07 23:31:42 +0900

    [#83] feat: 검색바 필터 기능 추가  (#87)
    * fix : 데이트 피커 모바일 때 range-end 수정
    
    * feat: 검색바 기능 추가
    
    * fix: 컴포넌트 분리

--- src/app/_components/Input/LocationInputView.tsx
+++ src/app/_components/Input/LocationInputView.tsx
@@ -4,8 +4,7 @@ import useMediaQueries from '@/hooks/useMediaQueries';
 import { IconLocation } from '@/public/svgs';
 import { useState } from 'react';
 import { Button, ModalForMobile, LocationDropdown } from '..';
-import { usePathname } from 'next/navigation';
-
+import * as Hangul from 'hangul-js';
 interface Field {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
@@ -21,17 +20,32 @@ interface Props {
 }
 
 function LocationInputView({
-  field: { value, ...field },
+  field: { onChange, value, ...field },
   locations,
 }: Props): JSX.Element {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState('');
+
+  const [filteredLocations, setFilteredLocations] = useState(locations);
   const mobileMediaQuery = useMediaQueries({ breakpoint: 767 })?.mediaQuery
     .matches;
   const isMobile = typeof window !== 'undefined' ? mobileMediaQuery : false;
 
   const handleClick = () => {
     setIsDropdownVisible(true);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event);
+    if (event.target.value) {
+      setFilteredLocations(
+        locations.filter(
+          (location) => Hangul?.search(location, event.target.value) !== -1,
+        ),
+      );
+    } else {
+      setFilteredLocations(locations);
+    }
   };
 
   const handleSelectLocation = (item: string) => {
@@ -41,7 +55,7 @@ function LocationInputView({
       },
     } as React.ChangeEvent<HTMLInputElement>;
 
-    field.onChange(event);
+    onChange(event);
     setIsDropdownVisible(false);
   };
 
@@ -59,8 +73,10 @@ function LocationInputView({
         <div className=' flex w-full  gap-4pxr'>
           <IconLocation className='absolute left-16pxr top-16pxr ' />
           <input
+          readOnly={isMobile ? true : false}
             autoComplete='off'
             {...field}
+            onChange={handleInputChange}
             value={value}
             placeholder='어디로 갈까요?'
             className=' w-full cursor-pointer whitespace-nowrap rounded-lg bg-gray100 py-16pxr pl-44pxr pr-16pxr text-black placeholder-gray500 outline-none font-body2-semibold placeholder:font-body2'
@@ -85,7 +101,7 @@ function LocationInputView({
           >
             <LocationDropdown
               isMobile={isMobile as boolean}
-              items={locations}
+              items={isMobile ? locations : filteredLocations}
               onSelect={handleChangeItem}
               activeItem={selectedItem}
               onClose={handleClose}
