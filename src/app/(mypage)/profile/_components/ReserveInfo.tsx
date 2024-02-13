ac7a4636a22b3e65694a7f8442607e02473a1c16
namminimi <111848336+namminimi@users.noreply.github.com>
2024-02-13 21:41:46 +0900

    [#109] feat: 마이페이지 Layout (#123)
    * chore: 폰트 수정
    
    * feat: 예약페이지 layout(상세보기쪽은 미완)
    
    * feat:header 쪽 유저 개인 옵션 선택 모달 구현
    
    * feat: mypage 폴더 구조 변경, mypage내에서 각 리스트 별로 폴더 생성, 각 리스트 클릭시 글자색 변경되게 기능 추가, 예약 상세보기 추가
    
    * fix: 마이페이지 선택항목 마우스 커서 변경되게 스타일 변경
    
    * chore: 사용하지 않는 잘못된 경로 제거
    
    * feat: 예약 상세 내역 반응형 완료
    
    * feat: 별점 구현
    
    * feat: profile 페이지 layout 완료
    
    * feat: 버튼 디자인 수정

--- src/app/(mypage)/profile/_components/ReserveInfo.tsx
+++ src/app/(mypage)/profile/_components/ReserveInfo.tsx
@@ -1,63 +1,35 @@
 'use client';
 
 import Button from '@/components/Button';
+import InfoAboutBookingPerson from '@/src/app/(header&footer)/reserve/_components/InfoAboutBookingPerson';
+import InfoAboutReserve from '@/src/app/(header&footer)/reserve/_components/InfoAboutReserve';
+import PaymentAmount from '@/src/app/(header&footer)/reserve/_components/PaymentAmount';
 import SiteInfo from '@/src/app/(header&footer)/reserve/_components/SiteInfo';
+import { useState } from 'react';
+import CancleReserverModal from './CancleReserverModal';
 
 function ReserveInfo() {
+  const [isClose, setIsClose] = useState(false);
+
+  const handleModal = () => {
+    setIsClose(!isClose);
+  };
+
   return (
     <>
-      <div className='flex-center justify-between'>
+      <div className='flex-center justify-between tabletMin:mb-32pxr'>
         <h2 className='hidden font-h1-semibold tabletMin:block'>예약 상세</h2>
-        <Button.Round custom='!w-108pxr bg-white border border-gray200 font-caption1-semibold !h-36pxr !text-gray500 !rounded-md'>
+        <Button.Round
+          custom='!w-108pxr bg-white border border-gray200 font-caption1-semibold !h-36pxr hidden tabletMin:flex !text-gray500 !rounded-md'
+          onClick={handleModal}
+        >
           예약 취소
         </Button.Round>
       </div>
-      <div className='flex flex-col gap-24pxr'>
-        <SiteInfo size='pc' />
-        <div className='flex flex-col gap-16pxr border-b border-gray200 pb-24pxr'>
-          <h3 className='text-black font-title3-semibold tabletMin:font-title1-semibold'>
-            예약 정보
-          </h3>
-          <ul className='flex flex-col gap-16pxr'>
-            <li className='flex items-center justify-start gap-53pxr text-gray500 font-caption1-semibold tabletMin:font-body2-semibold'>
-              인원
-              <span className='text-gray800 font-body2-semibold tabletMin:font-body1-bold'>
-                성인 99명, 아이 99명
-              </span>
-            </li>
-            <li className='flex items-center justify-start gap-53pxr text-gray500 font-caption1-semibold tabletMin:font-body2-semibold'>
-              일정
-              <span className='text-gray800 font-body2-semibold tabletMin:font-body1-bold'>
-                12.29(토) - 12.29(토)
-              </span>
-            </li>
-            <li className='flex items-center justify-start gap-53pxr text-gray500 font-caption1-semibold tabletMin:font-body2-semibold'>
-              애견
-              <span className='text-gray800 font-body2-semibold tabletMin:font-body1-bold'>
-                1마리
-              </span>
-            </li>
-          </ul>
-        </div>
-        <div className='flex flex-col gap-16pxr border-b border-gray200 pb-24pxr'>
-          <h3 className='text-black font-title3-semibold tabletMin:font-title1-semibold'>
-            예약자 정보
-          </h3>
-          <ul className='flex flex-col gap-16pxr'>
-            <li className='flex items-center justify-start gap-40pxr text-gray500 font-caption1-semibold tabletMin:font-body2-semibold'>
-              예약자명
-              <span className='text-gray800 font-body2-semibold tabletMin:font-body1-bold'>
-                홍길동
-              </span>
-            </li>
-            <li className='flex items-center justify-start gap-24pxr text-gray500 font-caption1-semibold tabletMin:font-body2-semibold'>
-              휴대폰 번호
-              <span className='text-gray800 font-body2-semibold tabletMin:font-body1-bold'>
-                010-1234-5678
-              </span>
-            </li>
-          </ul>
-        </div>
+      <div id='profile' className='flex flex-col gap-24pxr'>
+        <SiteInfo size='profile' />
+        <InfoAboutReserve />
+        <InfoAboutBookingPerson />
         <div className='flex flex-col gap-16pxr border-b border-gray200 pb-24pxr'>
           <h3 className='text-black font-title3-semibold tabletMin:font-title1-semibold'>
             차량 추가
@@ -73,68 +45,51 @@ function ReserveInfo() {
           <h3 className=' text-black font-title3-semibold tabletMin:font-title1-semibold'>
             추가 옵션
           </h3>
-          <ul>
-            <li>숫불 세트(고기2인분+식기)</li>
-            <li>장작 세트</li>
+          <ul className='flex flex-col gap-16pxr'>
+            <li className='text-gray800 font-body2-semibold tabletMin:font-body1-medium'>
+              숫불 세트(고기2인분+식기)
+            </li>
+            <li className='text-gray800 font-body2-semibold tabletMin:font-body1-medium'>
+              장작 세트
+            </li>
           </ul>
         </div>
-        <div className='flex justify-between'>
-          <div className='flex w-full flex-col gap-16pxr pb-24pxr'>
-            <h3 className=' text-black font-title3-semibold tabletMin:font-title1-semibold'>
+        <div className='flex flex-col justify-between tabletMin:flex-row'>
+          <div className='moblie:border-b flex w-full flex-col gap-16pxr pb-24pxr tabletMin:border-r tabletMin:pr-32pxr'>
+            <h3 className='text-black font-title3-semibold tabletMin:font-title1-semibold'>
               결제 정보
             </h3>
             <ul className='flex flex-col gap-12pxr'>
               <li className='flex-center justify-start gap-35pxr text-gray600 font-body2-medium'>
-                결제 일시
-                <span className='text-gray600 font-body2-semibold'>
+                <h4 className='whitespace-nowrap'>결제 일시</h4>
+                <span className='text-gray800 font-body2-medium'>
                   2024.12.28(토) 14:29
                 </span>
               </li>
               <li className='flex-center justify-start gap-35pxr text-gray600 font-body2-medium'>
-                결제 수단
-                <span className='text-gray600 font-body2-semibold'>
+                <h4 className='whitespace-nowrap'>결제 수단</h4>
+                <span className='text-gray800 font-body2-medium'>
                   현대 ****-****-**21
                 </span>
               </li>
             </ul>
           </div>
-          <div className='flex w-full flex-col gap-12pxr '>
-            <h3 className='text-black font-title3-semibold'>결제 금액</h3>
-            <ul className='flex flex-col gap-12pxr'>
-              <li className='flex-center justify-between text-gray600 font-body2-medium'>
-                객실 1개 x 2박
-                <span className='text-gray600 font-body2-semibold'>
-                  90,000원
-                </span>
-              </li>
-            </ul>
-            <ul className='flex flex-col gap-8pxr border-b-2 border-dashed pb-24pxr'>
-              <li className='flex-center justify-between text-gray600 font-body2-medium '>
-                추가 상품
-                <span className='text-gray600 font-body2-semibold'>
-                  합계 - 40,000원
-                </span>
-              </li>
-              <li className='flex-center justify-between text-gray600 font-body2-medium '>
-                바베큐 x 1
-                <span className='text-gray500 font-body2-semibold'>
-                  20,000원
-                </span>
-              </li>
-              <li className='flex-center justify-between text-gray600 font-body2-medium '>
-                장작 x 1
-                <span className='text-gray500 font-body2-semibold'>
-                  20,000원
-                </span>
-              </li>
-            </ul>
-            <div className='flex justify-between text-gray800 font-title1-bold'>
+          <div className='flex w-full flex-col gap-12pxr tabletMin:pl-32pxr'>
+            <PaymentAmount />
+            <div className='flex justify-between text-black font-body2-semibold'>
               <h2>총 결제금액</h2>
-              <h2 className='text-primary100'>130,000원</h2>
+              <h2>130,000원</h2>
             </div>
           </div>
+          <Button.Round
+            custom='w-full mt-40pxr mb-20pxr bg-white border border-gray200 font-caption1-semibold !h-36pxr flex tabletMin:hidden !text-gray700 !rounded-md'
+            onClick={handleModal}
+          >
+            예약 취소
+          </Button.Round>
         </div>
       </div>
+      {isClose && <CancleReserverModal handleClick={handleModal} />}
     </>
   );
 }
---
+++ src/app/(mypage)/profile/_components/Survey.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import Score from './Score';
+import Button from '@/components/Button';
+import { IconPlusNon } from '@/public/svgs';
+
+interface ReviewKeywordType {
+  id: number;
+  keyword: string;
+  isDone: boolean;
+}
+
+function Survey() {
+  const [keywords, setKeywords] = useState<ReviewKeywordType[]>([
+    { id: 1, keyword: '깨끗해요', isDone: false },
+    { id: 2, keyword: '조용해서 쉬기 좋아요', isDone: false },
+    { id: 3, keyword: '바베큐 해 먹기 좋아요', isDone: false },
+    { id: 4, keyword: '온수가 잘 나와요', isDone: false },
+    { id: 5, keyword: '매너타임이 잘 지켜져요', isDone: false },
+    { id: 6, keyword: '화장실이 잘 되어있어요', isDone: false },
+    { id: 7, keyword: '공용 시설 관리가 잘 되요', isDone: false },
+    { id: 8, keyword: '벌레 걱정 없어요', isDone: false },
+    { id: 9, keyword: '사진이 잘 나와요', isDone: false },
+    { id: 10, keyword: '냉난방이 잘 돼요', isDone: false },
+    { id: 11, keyword: '친절해요', isDone: false },
+    { id: 12, keyword: '편의시설이 잘 되어있어요', isDone: false },
+    { id: 13, keyword: '근처에 갈 곳이 많아요', isDone: false },
+  ]);
+  return (
+    <>
+      <div className='mt-24pxr flex flex-col gap-8pxr'>
+        <h3 className='text-black font-body1-bold'>전체 만족도는 어땠나요?</h3>
+        <Score />
+      </div>
+      <div className='mt-24pxr'>
+        <h3 className='text-black font-body1-bold'>어떤 점이 좋았나요?</h3>
+        <h4 className='mt-4pxr text-gray500 font-caption1-medium'>
+          이 캠핑장에 어울리는 키워드를 최대 3개까지 골라주세요
+        </h4>
+        <ul className='mypage mt-16pxr grid h-212pxr justify-between gap-12pxr overflow-hidden'>
+          {keywords.map((keyword) => {
+            return (
+              <Button.Round
+                key={keyword.id}
+                size='lg'
+                custom={`${keyword.isDone ? 'bg-primary50 ' : ''} hover:font-caption1-semibold !w-full !h-44pxr rounded-lg font-caption1-medium text-gray600`}
+              >
+                {keyword.keyword}
+              </Button.Round>
+            );
+          })}
+        </ul>
+        <div className='flex-center mt-12pxr'>
+          <button
+            /* onClick={() => handleAdd(vehicleNumber)} */
+            className='flex-center flex h-44pxr flex-nowrap gap-4pxr rounded-[99px] border border-gray300 px-20pxr py-12pxr font-body2-medium hover:bg-primary50'
+          >
+            <div className='h-20pxr w-20pxr'>
+              <IconPlusNon
+                width='100%'
+                height='100%'
+                viewBox='0 0 24 24'
+                fill='#949494'
+              />
+            </div>
+            <p className='whitespace-nowrap text-gray500 font-caption1-medium'>
+              더 보기
+            </p>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Survey;
