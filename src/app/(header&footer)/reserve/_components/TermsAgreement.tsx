8c2043f02859ae87b2116aa3561a442edd3b71d0
minhyeokG0 <yeppi9801@gmail.com>
2024-02-13 15:23:08 +0900

    feat: 클라이언트 컴포넌트 수정

--- src/app/(header&footer)/reserve/_components/TermsAgreement.tsx
+++ src/app/(header&footer)/reserve/_components/TermsAgreement.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 import { IconArrowDown, IconArrowRightNon } from '@/public/svgs';
 import { useAppDispatch, useAppSelector } from '@/hooks/redux';
