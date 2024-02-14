57b89409ec0f21604ff75ffe12756f521ebf9807
jyh0521 <jeonyh0521@gmail.com>
2024-02-15 01:34:24 +0900

    Feat: 디비 연동 수정

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,50 +1,26 @@
 import { NextResponse } from 'next/server';
-
-const conn = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-};
-const mysql = require('mysql');
-const connection = mysql.createConnection(conn);
-
-const executeQuery = (query: any, arrParams: any) => {
-  return new Promise((resolve, reject) => {
-    try {
-      connection.query(query, arrParams, (err: any, data: any) => {
-        if (err) {
-          console.log('Error in executing the query');
-          reject(err);
-        }
-        console.log('------db.jsx------');
-        //console.log(data)
-        resolve(data);
-      });
-    } catch (err) {
-      reject(err);
-    }
-  });
-};
+import { pool } from '../libs/mysql';
 
 export const GET = async () => {
-  await connection.connect();
+  try {
+    const db = await pool.getConnection();
 
-  let sql = 'SELECT * FROM user_info';
-  let result = await executeQuery(sql, []);
-  // await connection.query(sql, (error: any, data: any, field: any) => {
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     // console.log(data);
-  //     // result = data;
-  //     return NextResponse.json(data);
-  //   }
-  // });
+    const userId = 1;
 
-  // console.log(result);
+    const query = 'SELECT * FROM user_info WHERE user_id = ?';
+    const [rows] = await db.execute(query, [userId]);
 
-  return NextResponse.json(result);
-  // return NextResponse.json('test');
+    const query2 = 'SELECT * FROM user_info';
+    const [rows2] = await db.execute(query2, []);
+
+    db.release();
+    return NextResponse.json([rows, rows2]);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+      },
+      { status: 500 },
+    );
+  }
 };
