eeb5220d1fa04bff4c7581a5c7446db01550500d
aowjarkwk <justgam12@gmail.com>
2024-02-15 11:14:57 +0900

    [#73] 캠핑장 아이템 Link 태그가 캠핑장 이미지에만 적용 되도록  수정
    - 하트 아이콘 클릭 시 이동되던 현상

--- src/app/(header&footer)/_components/CampPlaceItem.tsx
+++ src/app/(header&footer)/_components/CampPlaceItem.tsx
@@ -1,10 +1,10 @@
 import Chip from '@/components/Chip';
-import { IconColoredHeart, IconHeart } from '@/public/svgs';
+import { IconColoredHeart } from '@/public/svgs';
 import Image from 'next/image';
 import Link from 'next/link';
-import { CampPlaceMockData } from './CampPlaceSection';
 import { useSearchParams } from 'next/navigation';
 import { formatDate } from '../../_utils/submitForSearchBar';
+import { CampPlaceMockData } from './CampPlaceSection';
 
 interface Props {
   campPlace: CampPlaceMockData;
@@ -29,12 +29,12 @@ function CampPlaceItem({ campPlace, isResponsive = false }: Props) {
   });
 
   return (
-    <li key={campPlace.id} className='w-full'>
-      <Link
-        href={`overview/${campPlace.id}?${newSearchParams.toString()}`}
-        className='flex w-full flex-col gap-16pxr'
-      >
-        <div className={`relative ${responsiveClasses}`}>
+    <li key={campPlace.id} className='flex w-full flex-col gap-16pxr'>
+      <div className='relative '>
+        <Link
+          href={`overview/${campPlace.id}?${newSearchParams.toString()}`}
+          className={`relative ${responsiveClasses}`}
+        >
           <Image
             width={262}
             height={262}
@@ -46,40 +46,37 @@ function CampPlaceItem({ campPlace, isResponsive = false }: Props) {
             src={campPlace.imgUrl}
             alt='캠핑장 이미지'
           />
-          <button
-            type='button'
-            className='group absolute bottom-16pxr right-16pxr h-34pxr w-34pxr rounded-full bg-gray800 bg-opacity-50 p-7pxr'
-          >
-            <IconHeart className='absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 transform group-hover:hidden' />
-            <IconColoredHeart className='absolute bottom-1/2 right-1/2 hidden translate-x-1/2 translate-y-1/2 transform group-hover:block' />
-          </button>
+        </Link>
+        <button
+          type='button'
+          className='absolute bottom-16pxr right-16pxr h-34pxr w-34pxr rounded-full bg-gray800 bg-opacity-50 fill-gray100 p-7pxr'
+        >
+          <IconColoredHeart className='absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 transform fill-none stroke-gray100 hover:fill-primary100 hover:stroke-primary100' />
+        </button>
+      </div>
+      <div className='flex flex-col gap-2pxr'>
+        <div className='flex gap-4pxr'>
+          <span className='overflow-hidden text-ellipsis whitespace-nowrap font-body2-semibold'>
+            {campPlace.placeName}
+          </span>
+          <span className='overflow-hidden text-ellipsis whitespace-nowrap font-medium text-gray500 font-caption1-medium'>
+            {campPlace.address}
+          </span>
         </div>
-        <div className='flex flex-col gap-2pxr'>
-          <div className='flex gap-4pxr'>
-            <span className='overflow-hidden text-ellipsis whitespace-nowrap font-body2-semibold'>
-              {campPlace.placeName}
+        <div className='flex flex-col gap-8pxr'>
+          <div>
+            <span className='text-gray800 font-title1-bold'>
+              ₩{campPlace.price.toLocaleString()}
             </span>
-            <span className='overflow-hidden text-ellipsis whitespace-nowrap font-medium text-gray500 font-caption1-medium'>
-              {campPlace.address}
-            </span>
+            <span className='text-20pxr text-gray800 font-title1'>원 부터</span>
           </div>
-          <div className='flex flex-col gap-8pxr'>
-            <div>
-              <span className='text-gray800 font-title1-bold'>
-                ₩{campPlace.price.toLocaleString()}
-              </span>
-              <span className='text-20pxr text-gray800 font-title1'>
-                원 부터
-              </span>
-            </div>
-            <div className='flex gap-10pxr'>
-              <Chip>힐링/휴식</Chip>
-              <Chip>자연</Chip>
-              <Chip>숲</Chip>
-            </div>
+          <div className='flex gap-10pxr'>
+            <Chip>힐링/휴식</Chip>
+            <Chip>자연</Chip>
+            <Chip>숲</Chip>
           </div>
         </div>
-      </Link>
+      </div>
     </li>
   );
 }
