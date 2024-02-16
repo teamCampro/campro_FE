bfd94c7b54de6840abd15f18daa16e776a366f3a
aowjarkwk <83692053+aowjarkwk@users.noreply.github.com>
2024-02-16 10:36:45 +0900

    [#74] fix: 온보딩 페이지 (#130)
    * [#74] 온보딩 페이지네이션 아이콘 변경
    
    * [#74] 선택 후 3초 뒤 메인 페이지로 이동
    
    - 로딩 이미지 변경

--- src/app/_components/Pagination.tsx
+++ src/app/_components/Pagination.tsx
@@ -1,4 +1,4 @@
-import { IconArrowLeft, IconArrowLeftGray } from '@/public/svgs';
+import { IconArrowLeftGray } from '@/public/svgs';
 
 interface Props {
   currentPage: number;
@@ -7,17 +7,19 @@ interface Props {
 }
 
 function Pagination({ currentPage, totalItems, onUpdatePage }: Props) {
+  const disabled = currentPage <= 1;
   return (
     <div className='flex w-103pxr items-center gap-16pxr'>
-      {currentPage > 1 ? (
-        <button type='button' onClick={() => onUpdatePage(currentPage - 1)}>
-          <IconArrowLeft />
-        </button>
-      ) : (
-        <button disabled>
+      {
+        <button
+          type='button'
+          disabled={disabled}
+          className={`${disabled ? 'text-gray400' : 'text-gray600'}`}
+          onClick={() => onUpdatePage(currentPage - 1)}
+        >
           <IconArrowLeftGray />
         </button>
-      )}
+      }
       <p className='pr-10pxr'>
         {currentPage} / {totalItems}
       </p>
