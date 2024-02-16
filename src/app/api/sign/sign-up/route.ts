ea654cf4bf1e54ed36294e54b5146f3a07a18ee1
kontae <koet0928@gmail.com>
2024-02-16 16:22:22 +0900

    Fix: 카멜케이스 통일 및 cnt 조건 명시

--- src/app/api/sign/sign-up/route.ts
+++ src/app/api/sign/sign-up/route.ts
@@ -13,20 +13,20 @@ export const POST = async (req: NextRequest) => {
   try {
     const userData = await req.json();
     const db = await pool.getConnection();
-    const user_email = userData.email;
+    const userEmail = userData.email;
     const query = 'SELECT COUNT(*) AS cnt FROM user_info WHERE email = ?';
-    const [rows] = await db.execute(query, [user_email]);
+    const [rows] = await db.execute(query, [userEmail]);
     let tmpResult: any = rows;
     const exists = tmpResult[0].cnt;
 
-    if (!exists) {
+    if (exists === 0) {
       const hashingPW = await hashedPassword(userData.password);
       const query2 = 'INSERT INTO user_info (email, phone, password, role, nickname) VALUES (?, ?, ?, ?, ?)';
       await db.execute(query2, [userData.email, userData.phone, hashingPW, userData.role, userData.nickname]);
       db.release();
       return NextResponse.json({});
     }
-    
+
     else {
       const message = '입력하신 이메일은 이미 존재하는 이메일입니다.'
       return NextResponse.json({ error: message, }, { status: 500 },);
