9bcbe6259c5eafc6048d232d6fc8b54abc3199bc
jyh0521 <jeonyh0521@gmail.com>
2024-02-17 18:36:52 +0900

    Feat: user 정보 불러오는 api 추가

---
+++ src/app/api/user/[userId]/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { pool } from '../../libs/mysql';
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { userId: string } },
+) => {
+  try {
+    const db = await pool.getConnection();
+
+    const { userId } = params;
+
+    const query = 'SELECT * FROM user_info WHERE id = ?';
+    const [row] = await db.execute(query, [userId]);
+
+    let userInfo: any = row;
+
+    const result = {
+      name: userInfo[0].name,
+      email: userInfo[0].email,
+      phone: userInfo[0].phone,
+      role: userInfo[0].role,
+      nickname: userInfo[0].nickname,
+    };
+
+    db.release();
+    return NextResponse.json({ result });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+      },
+      { status: 500 },
+    );
+  }
+};
