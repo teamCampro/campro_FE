3df77cc596a0ad388c1bf6986ec3ecad7673dd87
jyh0521 <jeonyh0521@gmail.com>
2024-02-17 15:53:32 +0900

    [backend] display_address => displayAddress로 내려가게 수정

--- src/app/api/camping-zone/main-list/route.ts
+++ src/app/api/camping-zone/main-list/route.ts
@@ -6,13 +6,13 @@ export const GET = async () => {
     const db = await pool.getConnection();
 
     const [recommendList] = await db.execute(
-      'SELECT id, name, display_address, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone LIMIT 7',
+      'SELECT id, name, display_address as displayAddress, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone LIMIT 7',
     );
     const [popularList] = await db.execute(
-      'SELECT id, name, display_address, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone LIMIT 7',
+      'SELECT id, name, display_address as displayAddress, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone LIMIT 7',
     );
     const [recentList] = await db.execute(
-      'SELECT id, name, display_address, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone ORDER BY created_at DESC LIMIT 7',
+      'SELECT id, name, display_address as displayAddress, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone ORDER BY created_at DESC LIMIT 7',
     );
 
     db.release();
