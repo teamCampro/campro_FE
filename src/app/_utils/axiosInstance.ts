9a707409a0eb31d55632858cf0caf5d460cfbeb2
wade <90304025+wecaners@users.noreply.github.com>
2024-02-19 16:14:35 +0900

    [#162] feat: 로그인 API 연동 (#163)
    * feat: 로그인 API 연동
    
    * feat: 로그인 api 환경 변수 수정

--- src/app/_utils/axiosInstance.ts
+++ src/app/_utils/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   timeout: 5000,
 });
