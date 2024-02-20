15b76e08f042bcabd4132b57f883c447f7aa5531
kontae <koet0928@gmail.com>
2024-02-20 15:49:36 +0900

    Fix: conflict 오류 해결

--- src/app/api/owner/camping-zone/register/route.ts
+++ src/app/api/owner/camping-zone/register/route.ts
@@ -5,7 +5,6 @@ export const POST = async (req: NextRequest) => {
     try {
         const czData = await req.json();
         const db = await pool.getConnection();
-        
         const seasonString = JSON.stringify(czData.season);
         const facilitiesString = JSON.stringify(czData.facilities);
         const campSubImagesString = JSON.stringify(czData.campSubImages);
@@ -14,22 +13,18 @@ export const POST = async (req: NextRequest) => {
         INSERT INTO camping_zone (name, tel, boss_email, business_number, tour_number, facilities,
             season, open_day, address, camp_image, plan_image, intro)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-        
         const czInsertResult: any = await db.execute(czInsertQuery, [czData.name, czData.tel, czData.bossEmail, czData.businessNumber,
         czData.tourNumber, facilitiesString, seasonString, czData.openDay, czData.address, czData.campImage,
         czData.planImage, czData.intro]);
-        
+
         const czDataImageId = czInsertResult[0].insertId;
         const czImageInsertQuery = `
         INSERT INTO camping_zone_sub_image (camping_zone_id, img_urls) VALUES (?, ?)`;
-        
-        await db.execute(czImageInsertQuery, [czDataImageId, campSubImagesString]);
 
+        await db.execute(czImageInsertQuery, [czDataImageId, campSubImagesString]);
         db.release();
         return NextResponse.json({});
     } catch (error) {
-        return NextResponse.json(
-            { error, }, { status: 500 },
-        );
+        return NextResponse.json({ error }, { status: 500 });
     }
 };
