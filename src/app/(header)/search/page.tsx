2b058c2e1511d067e6dd31432587ca09f7f0c8c0
aowjarkwk <justgam12@gmail.com>
2024-02-22 14:52:48 +0900

    [#188] 로딩 여부를 186번 pr의 로직으로 변경

--- src/app/(header)/search/page.tsx
+++ src/app/(header)/search/page.tsx
@@ -10,7 +10,7 @@ import {
   SortDropdown,
 } from '@/components/index';
 import usePagination from '@/hooks/usePagination';
-import { Suspense, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { axiosInstance } from '../../_utils/axiosInstance';
 import kakaoMarkerGenerator from '../../_utils/kakaoMarkerGenerator';
 import KakaoMap from './_components/KakaoMap';
@@ -118,59 +118,57 @@ function Page({ searchParams }: SearchParamsType) {
           <SearchFilter />
         </div>
       </div>
-      <Suspense fallback={<div>미녀기</div>}>
-        <div className='flex-center searchPageOverFlow h-full w-full'>
-          {mapSize !== 'map' && (
-            <div
-              className={`scrollbar-hide flex h-full pb-40pxr ${mapSize === 'half' ? 'grow-0' : 'grow-0 basis-auto'}  basis-776pxr flex-col gap-24pxr overflow-y-scroll px-40pxr pb-40pxr pt-16pxr mobile:px-16pxr tablet:grow-1 tablet:px-40pxr mobile767:grow-1 mobile767:basis-412pxr tablet1002:basis-420pxr tablet1199:basis-622pxr ${mapSize === 'half' ? 'desktop1440:max-w-1132pxr desktop1440:flex-grow-7 desktop1440:basis-776pxr' : ''}`}
-            >
-              <div className='flex items-center justify-around'>
-                <h3 className='text-black font-title1-semibold mobile:font-body1-semibold'>
-                  전체 {campPlaceData?.length || 0}
-                </h3>
-                <SortDropdown />
-              </div>
-
-              <div className='flex w-full flex-col gap-48pxr mobile:gap-64pxr tablet:gap-64pxr'>
-                {campPlaceData.length > 0 ? (
-                  <>
-                    <CampSearchList
-                      currentPage={currentPage}
-                      campPlaces={campPlaceData}
-                      gridColumns={mapBasis[mapSize].list}
-                    />
-                    <SearchPagination
-                      currentPage={currentPage}
-                      totalItems={totalItems}
-                      onUpdatePage={updateCurrentPage}
-                    />
-                  </>
-                ) : (
-                  <div className='flex-center h-500pxr flex-col'>
-                    <h3 className='text-gray600 font-h2-semibold'>
-                      모든 캠핑장 예약이 마감됐어요
-                    </h3>
-                    <div className='text-gray500 font-body1-medium'>
-                      다른 일정/지역으로 재검색 해보는 건 어떨까요?
-                    </div>
-                  </div>
-                )}
-              </div>
-            </div>
-          )}
+      <div className='flex-center searchPageOverFlow h-full w-full'>
+        {mapSize !== 'map' && (
           <div
-            className={`relative h-full shrink-0 grow-1 tablet1002:basis-348pxr tablet1199:basis-381pxr desktop1440:basis-664pxr ${mapBasis[mapSize].map}`}
+            className={`scrollbar-hide flex h-full pb-40pxr ${mapSize === 'half' ? 'grow-0' : 'grow-0 basis-auto'}  basis-776pxr flex-col gap-24pxr overflow-y-scroll px-40pxr pb-40pxr pt-16pxr mobile:px-16pxr tablet:grow-1 tablet:px-40pxr mobile767:grow-1 mobile767:basis-412pxr tablet1002:basis-420pxr tablet1199:basis-622pxr ${mapSize === 'half' ? 'desktop1440:max-w-1132pxr desktop1440:flex-grow-7 desktop1440:basis-776pxr' : ''}`}
           >
-            <KakaoMap
-              region={searchParams.location}
-              map={map}
-              setMap={setKakaoMap}
-              mapSize={mapSize}
-              campPlaceData={campPlaceData}
-            />
+            <div className='flex items-center justify-around'>
+              <h3 className='text-black font-title1-semibold mobile:font-body1-semibold'>
+                전체 {campPlaceData?.length || 0}
+              </h3>
+              <SortDropdown />
+            </div>
+
+            <div className='flex w-full flex-col gap-48pxr mobile:gap-64pxr tablet:gap-64pxr'>
+              {campPlaceData.length > 0 ? (
+                <>
+                  <CampSearchList
+                    currentPage={currentPage}
+                    campPlaces={campPlaceData}
+                    gridColumns={mapBasis[mapSize].list}
+                  />
+                  <SearchPagination
+                    currentPage={currentPage}
+                    totalItems={totalItems}
+                    onUpdatePage={updateCurrentPage}
+                  />
+                </>
+              ) : (
+                <div className='flex-center h-500pxr flex-col'>
+                  <h3 className='text-gray600 font-h2-semibold'>
+                    모든 캠핑장 예약이 마감됐어요
+                  </h3>
+                  <div className='text-gray500 font-body1-medium'>
+                    다른 일정/지역으로 재검색 해보는 건 어떨까요?
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
+        )}
+        <div
+          className={`relative h-full shrink-0 grow-1 tablet1002:basis-348pxr tablet1199:basis-381pxr desktop1440:basis-664pxr ${mapBasis[mapSize].map}`}
+        >
+          <KakaoMap
+            region={searchParams.location}
+            map={map}
+            setMap={setKakaoMap}
+            mapSize={mapSize}
+            campPlaceData={campPlaceData}
+          />
         </div>
-      </Suspense>
+      </div>
     </>
   );
 }
