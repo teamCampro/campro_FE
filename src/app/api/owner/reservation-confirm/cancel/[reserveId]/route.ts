7bdb54ad2292b2fda47faf04757a0a63d523db39
kontae <koet0928@gmail.com>
2024-02-23 19:42:37 +0900

    Feat: 사장님 페이지 예약 취소 api 추가

---
+++ src/app/api/owner/reservation-confirm/cancel/[reserveId]/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { pool } from "@/src/app/api/libs/mysql";
+
+export const PATCH = async (
+    req: NextRequest,
+    { params }: { params: { reserveId: string } },
+) => {
+    try {
+        const rStatusData = await req.json();
+        const db = await pool.getConnection();
+        const { reserveId } = params;
+        const reservationStatus = rStatusData.status;
+
+        if (reservationStatus === 'RESERVE_WAITING') {
+            const query = `UPDATE reservation SET status = 'RESERVE_CANCEL' WHERE id = ?`;
+            await db.execute(query, [reserveId]);
+
+            db.release();
+            return NextResponse.json({});
+        }
+
+        else {
+            const statusErrorMessage = '예약 대기 상태 아님';
+            return NextResponse.json({ error: statusErrorMessage }, { status: 500 });
+        }
+
+    }
+    catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
