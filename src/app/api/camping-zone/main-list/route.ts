c378ea0caec726ce956f1a0d16eda9e54f94db5f
jyh0521 <jeonyh0521@gmail.com>
2024-02-24 19:33:02 +0900

    Feat: 메인 페이지 캠핑장 목록 최소 금액 쿼리로 뜨게 수정

--- src/app/api/camping-zone/main-list/route.ts
+++ src/app/api/camping-zone/main-list/route.ts
@@ -6,13 +6,38 @@ export const GET = async () => {
     const db = await pool.getConnection();
 
     const [recommendList] = await db.execute(
-      'SELECT id, name, display_address as displayAddress, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone LIMIT 7',
+      `SELECT id, name, display_address as displayAddress, camp_image as campImage, a.minimumAmount, keyword
+        FROM camping_zone cz
+      INNER JOIN (
+      SELECT czs.camping_zone_id, MIN(czs.price) minimumAmount
+        FROM camping_zone cz
+      INNER JOIN camping_zone_site czs ON cz.id = czs.camping_zone_id
+      GROUP BY czs.camping_zone_id
+      ) a ON cz.id = a.camping_zone_id
+      LIMIT 7`,
     );
     const [popularList] = await db.execute(
-      'SELECT id, name, display_address as displayAddress, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone LIMIT 7',
+      `SELECT id, name, display_address as displayAddress, camp_image as campImage, a.minimumAmount, keyword
+      FROM camping_zone cz
+     INNER JOIN (
+    SELECT czs.camping_zone_id, MIN(czs.price) minimumAmount
+      FROM camping_zone cz
+     INNER JOIN camping_zone_site czs ON cz.id = czs.camping_zone_id
+     GROUP BY czs.camping_zone_id
+     ) a ON cz.id = a.camping_zone_id
+     LIMIT 7`,
     );
     const [recentList] = await db.execute(
-      'SELECT id, name, display_address as displayAddress, camp_image as campImage, 40000 as minimumAmount, keyword FROM camping_zone ORDER BY created_at DESC LIMIT 7',
+      `SELECT id, name, display_address as displayAddress, camp_image as campImage, a.minimumAmount, keyword
+      FROM camping_zone cz
+     INNER JOIN (
+    SELECT czs.camping_zone_id, MIN(czs.price) minimumAmount
+      FROM camping_zone cz
+     INNER JOIN camping_zone_site czs ON cz.id = czs.camping_zone_id
+     GROUP BY czs.camping_zone_id
+     ) a ON cz.id = a.camping_zone_id
+     ORDER BY created_at DESC
+     LIMIT 7`,
     );
 
     db.release();
