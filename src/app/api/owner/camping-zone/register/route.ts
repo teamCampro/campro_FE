349ecd2438f517107ff82d69014fecbcb98101ba
kontae <koet0928@gmail.com>
2024-02-27 23:29:05 +0900

    Feat: 캠핑장 등록 데이터 추가

--- src/app/api/owner/camping-zone/register/route.ts
+++ src/app/api/owner/camping-zone/register/route.ts
@@ -10,15 +10,19 @@ export const POST = async (req: NextRequest) => {
     const campSubImagesString = JSON.stringify(czData.campSubImages);
 
     const czInsertQuery = `
-        INSERT INTO camping_zone (name, tel, boss_id, boss_email, business_number, tour_number, facilities,
-            onboarding_keyword, address, camp_image, plan_image, manner_time_start, manner_time_end,
-            open_time, intro, guide, refund_guide)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        INSERT INTO camping_zone (name, lat, lng, tel, display_address, boss_id, boss_email,
+            boss_address, business_number, tour_number, facilities, onboarding_keyword, address,
+            camp_image, plan_image, manner_time_start, manner_time_end, open_time, intro, guide, refund_guide)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
     const czInsertResult: any = await db.execute(czInsertQuery, [
       czData.name,
+      czData.lat,
+      czData.lng,
       czData.tel,
+      czData.displayAddress,
       czData.bossId,
       czData.bossEmail,
+      czData.bossAddress,
       czData.businessNumber,
       czData.tourNumber,
       facilitiesString,
